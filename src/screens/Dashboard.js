72ea5a620cdbcb75702b9fe24df4c30abd319a0b
David <davidnickromero@gmail.com>
2021-10-21 11:38:49 -0700

    Updated template

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -11,8 +11,7 @@ export default function Dashboard({ navigation }) {
       <Logo />
       <Header>Let’s start</Header>
       <Paragraph>
-        Your amazing app starts here. Open you favorite code editor and start
-        editing this project.
+        You logged in, now it's time to make some new friends!
       </Paragraph>
       <Button
         mode="outlined"
