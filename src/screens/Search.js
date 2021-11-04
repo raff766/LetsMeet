ecfbb53b31adf6cb7e0bc01bc730c8b284b04003
David <davidnickromero@gmail.com>
2021-11-03 21:51:06 -0700

    GoogleMaps for search tab

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { View } from 'react-native'
+import { WebView } from 'react-native-webview'
 import { Text } from 'react-native-paper'
 import GlobalStyles from '../core/GlobalStyles'
 
 export default () => {
   return (
     <View style={GlobalStyles.background}>
-      <View style={{ justifyContent: 'center', flex: 1 }}>
-        <Text>Search</Text>
-      </View>
-    </View>
+      <WebView
+        style={{ width: 420, height: 230, flex: 1}}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        source={{ uri: "https://www.google.com/maps/@34.2449095,-118.5258363,13.79z" }}
+      />
+    </View>    
   )
 }
