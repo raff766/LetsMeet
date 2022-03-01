e26c6e898523fe6dfe73d8a7343af386e9e0b276
David <davidnickromero@gmail.com>
2022-03-01 00:58:27 -0800

    Added setup for remove button

--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -1,14 +1,34 @@
 import React from 'react'
-import { Text } from 'react-native-paper'
+import { Button, Text } from 'react-native-paper'
 import { View } from 'react-native'
 import GlobalStyles from '../styles/GlobalStyles'
 import { StyleSheet } from 'react-native'
+import { UserContext } from '../core/UserContext'
 
 export default function EventScreen({ route }) {
   const eventData = route.params
+  
+  const onSubmit = useCallback(() => {
+    const { userId } = useContext(UserContext)
+    sendData(REQUEST.EVENT, {
+      userid: userId,
+      eventid: eventData["eventid"],
+    })
+    navigation.goBack()
+  }, )
+
   return (
-    <View style={{ ...GlobalStyles.background, alignItems: 'flex-start' }}>
-      <Text style={GlobalStyles.header}>{eventData.name}</Text>
+    <View style={{ ...GlobalStyles.background, alignItems: 'flex-start'}}>
+      <View style={{
+          width: '100%',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <Text style={GlobalStyles.header}>{eventData.name}</Text>
+        <Button color='red' onPress={() => console.log("Leave Button Pressed")}>
+          Leave
+        </Button>
+      </View>
       <View style={styles.eventInfo}>
         <Text>Host: {eventData.host.name}</Text>
         <Text>Date: {eventData.date}</Text>
