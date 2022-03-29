99a258fdbf40bf53c57ad558ceaefa2ea94bc907
Raffi <raff766>
2022-03-29 13:47:05 -0700

    Added time to creating events

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -5,21 +5,47 @@ import DateTimePicker from '@react-native-community/datetimepicker'
 
 export default function DatePicker({ onPicked }) {
   const [date, setDate] = useState(new Date())
-  const [show, setShow] = useState(false)
+  const [showDate, setShowDate] = useState(false)
+  const [showTime, setShowTime] = useState(false)
 
-  const onChange = (event, selectedDate) => {
+  const onChooseDate = (event, selectedDate) => {
     const currentDate = selectedDate || date
-    setShow(Platform.OS === 'ios')
+    setShowDate(Platform.OS === 'ios')
+    setDate(currentDate)
+    setShowTime(true)
+    onPicked(currentDate)
+  }
+
+  const onChooseTime = (event, selectedTime) => {
+    const currentDate = date
+    currentDate.setTime(selectedTime.getTime())
+    setShowTime(Platform.OS === 'ios')
     setDate(currentDate)
     onPicked(currentDate)
+  }
+
+  function formatAMPM(dateToFormat) {
+    let hours = dateToFormat.getHours()
+    let minutes = dateToFormat.getMinutes()
+    const ampm = hours >= 12 ? 'pm' : 'am'
+    hours %= 12
+    hours = hours || 12 // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes
+    const strTime = hours + ':' + minutes + ' ' + ampm
+    return strTime
   }
 
   return (
     <View>
       <View>
-        <Button onPress={() => setShow(true)}>{date.toDateString()}</Button>
+        <Button onPress={() => setShowDate(true)}>
+          {date.toLocaleDateString() + ' ' + formatAMPM(date)}
+        </Button>
       </View>
-      {show && <DateTimePicker value={date} onChange={onChange} />}
+      {showDate && <DateTimePicker value={date} onChange={onChooseDate} />}
+      {showTime && (
+        <DateTimePicker value={date} onChange={onChooseTime} mode="time" />
+      )}
     </View>
   )
 }
