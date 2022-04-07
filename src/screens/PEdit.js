1733eac821b1f5e9fe9b3ff516c4d1758d712b1a
MauricioCasti <71904932+MauricioCasti@users.noreply.github.com>
2022-04-06 20:50:15 -0700

    added profile and profile edit page, added styles into GlobalStyles

---
+++ src/screens/PEdit.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Text, View, Image, TextInput} from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import Profile from './Profile'
+import { profilestyles } from '../../styles/ProfileStyle'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+
+// Allows to edit thet profile screen
+
+export default function PEdit({ route }){
+let username = route.params
+    return(
+        <View style={profilestyles.pcontainer}>
+            <TouchableOpacity onPress={()=>{}}>
+                <Image style={profilestyles.avataredit} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+                <Text style={{alignSelf: 'center'}}>Tap to Edit Image</Text>
+            </TouchableOpacity>
+            <Text style={{fontSize: 20, alignItems: 'center', margin: 10}}>{username}</Text>
+            <View>
+                <View style={profilestyles.action}>
+                    <Icon name='account' color='black' size={25}/>
+                    <TextInput
+                        placeholder='User Name'
+                        placeholderTextColor='#666666'
+                        autoCorrect={false}
+                        style={profilestyles.textInput}
+                    />
+                </View>
+                <View style={profilestyles.action}>
+                    <Icon name='account' color='black' size={25}/>
+                    <TextInput
+                        placeholder='Full Name'
+                        placeholderTextColor='#666666'
+                        autoCorrect={false}
+                        style={profilestyles.textInput}
+                    />
+                </View>
+                {/* make bio container */}
+                <View style={profilestyles.action}>
+                    <Icon name='lead-pencil' color='black' size={25}/>
+                    <TextInput
+                        placeholder='Bio/Description'
+                        placeholderTextColor='#666666'
+                        autoCorrect={false}
+                        style={profilestyles.textInput}
+                    />
+                </View>
+            </View>
+        </View>
+
+    );
+}
---
+++ src/screens/Profile.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Text, View, Image} from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { profilestyles } from '../../styles/ProfileStyle'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
+export default function Profile({route, navigation }) {
+  let name = route.params
+    return (
+      <View>
+          <View style={profilestyles.header}>
+            <TouchableOpacity
+            onPress={()=> navigation.navigate("PEdit", name)}>
+              <Icon name='account-edit' size={50} style={profilestyles.editlogo}/> 
+            </TouchableOpacity>
+          </View>
+          {/* Base image as profile screen, later on would take photo from phone or camera */}
+         <Image style={profilestyles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+          <View style={profilestyles.body}>
+            <View>
+                <Text style={profilestyles.info}>- College Student at Cal State - Northridge</Text>
+            </View>
+            <View>    
+                <Text style={profilestyles.description}>- Likes Music, Hanging out with friends and sleeping.</Text>
+            </View>
+            <View style={profilestyles.bodyContent}>
+              {/*               
+              <TouchableOpacity style={profilestyles.buttonContainer}>
+                <Text>College Student</Text> 
+              </TouchableOpacity>
+              <TouchableOpacity style={profilestyles.buttonContainer}>
+                <Text>Outgoing</Text> 
+              </TouchableOpacity> */}
+            </View>
+        </View>
+      </View>
+    );
+}
