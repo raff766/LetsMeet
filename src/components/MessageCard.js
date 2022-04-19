393922f5e224c381f94fe52eb18a439238d35c0e
David <davidnickromero@gmail.com>
2022-04-19 02:18:39 -0700

    Added SocketIO to send messages

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
-export default function MessageCard({ name, message, convoId, navigation }) {
+export default function MessageCard({ name, message, convoId, userId, navigation }) {
   return (
     <View>
       <TouchableOpacity
-        onPress={() => navigation.navigate('ChatScreen', { name, convoId } )}
+        onPress={() => navigation.navigate('ChatScreen', { name, convoId, userId } )}
         style={styles.message}>
         <Text style={{ ...styles.text, fontWeight: 'bold' }}>{name}</Text>
         <Text style={styles.text}>{message}</Text>
